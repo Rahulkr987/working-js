ff4947ffa94407d31e1b7d70328c5aebfc5c8277
Rahulkr987 <122821644+Rahulkr987@users.noreply.github.com>
2023-09-09 21:23:30 +0530

    Add files via upload

---
+++ working-javascript.js
@@ -0,0 +1,49 @@
+//1 ++++++++++++++++++++++++++
+console.log(addNo(10,20));
+function addNo(num1 , num2){
+    return num1 +num2
+}
+
+//2 ***************************************
+
+
+console.log(multiplyNumber(10 , 20));
+ const multiplyNumber = function( num3, num4){
+return num3*num4
+}
+
+// 3 ====================================
+
+function addnumber (d1 , d2){
+    var sum
+    sum = d1 + d2;
+    return sum ;
+
+}
+console.log(addnumber(1,2));
+
+//4====================================
+
+{
+console.log(x);
+console.log(y);
+console.log(z);
+
+let x = "sr" ;
+var y = 'software';
+const z = "developer";
+
+
+console.log(x);
+console.log(y);
+console.log(z);
+}
+
+// ========================
+
+{
+    console.log(value)
+    let value = " hi bye"
+    console.log(value)
+
+}
